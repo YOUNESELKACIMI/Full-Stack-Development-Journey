d08d7cb72b2205d0476fd38bc96133cec8d82e5a
YOUNES EL KACIMI <youns.elkacimi@gmail.com>
2023-12-09 12:45:46 +0100

    Exercice 1.9

--- Part1/courseinfo/src/App.jsx
+++ Part1/courseinfo/src/App.jsx
@@ -1,3 +1,11 @@
+import { useState } from "react"
+
+
+
+
+
+
+
 const Header = (props) =>{
   
   return (
@@ -38,6 +46,37 @@ const Total = (props) => {
 }
 
 
+
+
+
+const Display = ({counter}) => <div> {counter} </div> 
+
+
+
+
+const Button = ({ onSmash, text }) => <button onClick={onSmash}>{text}</button>
+
+
+const History = ({allClicks}) => {
+
+  if(allClicks.length==0){
+    return (
+      <div>the application works by clicking</div>
+    )
+  }
+  
+    return (
+      <div>
+          <br></br>
+          <h1>Button Press history : {allClicks.join(' ')}</h1>
+      </div>
+    )
+}
+
+
+
+
+
 const App = () => {
 
   const course = {
@@ -58,11 +97,90 @@ const App = () => {
     ]
   }
 
+
+  const [counter,setCounter] = useState(2)
+
+const increment = ( ) => setCounter(counter+1)
+const reset = () => setCounter(0)
+const decrement = () => setCounter(counter -1)
+console.log("rendering ...",counter)
+
+
+
+const [right,setRight] = useState(0)
+const [left,setLeft] =  useState(0)
+
+const [clicks,setClicks] = useState({
+  left:0,
+  right:0
+})
+
+const [allClicks,setAll] = useState([])
+const [total, setTotal] =  useState(0)
+
+
+const HandleLeftClick = () => {
+
+  setAll(allClicks.concat('L'))
+
+  const newLeftClick = {
+    
+    ...clicks,
+    
+    left: clicks.left+1
+  }
+  const updatedLeft = newLeftClick.left
+
+  setTotal(updatedLeft+newLeftClick.right)
+
+  setClicks(newLeftClick)
+
+
+}
+
+const HandleRightClick = () => {
+
+  setAll(allClicks.concat('R'))
+
+  const newRightClick = {
+    ...clicks,
+    right:clicks.right+1
+  }
+
+  const updatedRight = newRightClick.right
+  setTotal(updatedRight+newRightClick.left)
+  setClicks(newRightClick)
+}
+
+
   return (
     <div>
+      <h1><Display counter = {counter} /></h1>
+      <Button onSmash={increment} text = "increment"/>
+      <Button onSmash={reset} text = "reset"/>
+      <Button onSmash={decrement} text = "decrement"/>
       <Header course = {course.name} /> 
       <Content parts={course.parts}/>
       <Total parts={course.parts}/>
+
+
+    <h1>Complex State</h1>
+    <table>
+      <tr>
+        <th>{clicks.left}</th>
+        <th>{clicks.right}</th>
+      </tr>
+      <tr>
+        <td><button onClick={HandleLeftClick} >Left</button></td>
+        <td><button onClick={HandleRightClick} >Right</button></td>
+      </tr>
+    </table>
+
+    
+
+    <History allClicks={allClicks}/>
+
+    <h1>{total}</h1>
     </div>
   )
 }
--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -26,41 +26,47 @@ const Statistics = ({good,neutral,bad}) => {
   console.log("all = ",all)
   const average = (all>0) ? (good-bad)/all:0
   const positive  = (all>0) ? (good / all)*100:0
+  if(all==0) return (
+    <div>No feedback given</div>
 
-  return (
-    <div>
-        <table>
-          <tbody>
-            <tr>
-              <td>good</td>
-              <td>{good}</td>
-            </tr>
-            <tr>
-              <td>neutral</td>
-              <td>{neutral}</td>
-            </tr>
-            <tr>
-              <td>bad</td>
-              <td>{bad}</td>
-            </tr>
-            <tr>
-              <td>all</td>
-              <td>{all}</td>
-            </tr>
-            <tr>
-              <td>average</td>
-              <td>{average}</td>
-            </tr>
-            <tr>
-              <td>positive</td>
-              <td>{positive} %</td>
-            </tr>
-          </tbody>
-        </table>
-
-
-    </div>
   )
+
+  else {
+    return (
+      <div>
+          <table>
+            <tbody>
+              <tr>
+                <td>good</td>
+                <td>{good}</td>
+              </tr>
+              <tr>
+                <td>neutral</td>
+                <td>{neutral}</td>
+              </tr>
+              <tr>
+                <td>bad</td>
+                <td>{bad}</td>
+              </tr>
+              <tr>
+                <td>all</td>
+                <td>{all}</td>
+              </tr>
+              <tr>
+                <td>average</td>
+                <td>{average}</td>
+              </tr>
+              <tr>
+                <td>positive</td>
+                <td>{positive} %</td>
+              </tr>
+            </tbody>
+          </table>
+
+
+      </div>
+    )
+  }
 }
 
 

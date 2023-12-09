81172739020e6c0bf359933c4f65ea5acb74d955
YOUNES EL KACIMI <youns.elkacimi@gmail.com>
2023-12-09 12:42:53 +0100

    Exercice 1.8

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -21,29 +21,14 @@ const Button = ({ onSmash, text }) => <button onClick={onSmash}>{text}</button>
 
 
 
-
-
-
-const App = () => {
-  const [good,setGood] = useState(0)
-  const [neutral,setNeutral] = useState(0)
-  const [bad,setBad] = useState(0)
-  
+const Statistics = ({good,neutral,bad}) => {
   const all = good + neutral + bad
   console.log("all = ",all)
   const average = (all>0) ? (good-bad)/all:0
   const positive  = (all>0) ? (good / all)*100:0
-  
+
   return (
     <div>
-        <Header text="Give feedback"/>
-        
-        <Button onSmash={() => setGood(good+1)} text="good"/>
-        <Button onSmash={() => setNeutral(neutral+1)} text="neutral"/>
-        <Button onSmash={() => setBad(bad+1)} text="bad"/>
-
-        <Header text="Statistics" />
-
         <table>
           <tbody>
             <tr>
@@ -74,7 +59,31 @@ const App = () => {
         </table>
 
 
+    </div>
+  )
+}
 
+
+
+
+const App = () => {
+  const [good,setGood] = useState(0)
+  const [neutral,setNeutral] = useState(0)
+  const [bad,setBad] = useState(0)
+  
+
+  return (
+    <div>
+        <Header text="Give feedback"/>
+        
+        <Button onSmash={() => setGood(good+1)} text="good"/>
+        <Button onSmash={() => setNeutral(neutral+1)} text="neutral"/>
+        <Button onSmash={() => setBad(bad+1)} text="bad"/>
+
+        <Header text="Statistics" />
+
+        <Statistics good={good} neutral={neutral} bad={bad}  />
+ 
     </div>
   )
 }

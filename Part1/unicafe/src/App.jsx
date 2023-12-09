f7eeca3fdd7b8045d0c461e141a4d10f00b51541
YOUNES EL KACIMI <youns.elkacimi@gmail.com>
2023-12-09 10:43:21 +0100

    Exercice 1.6

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -14,64 +14,11 @@ const Header = (props) =>{
     </div>
   )
 }
-const Part = (props) => {
-
-  return (
-    <div>
-      <p>{props.content} {props.exercices} </p>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  const {parts} = props;
-  console.log(parts)
-  return (
-    <div>
-    <Part content = "Fundamentals of React" exercices = {parts[0].exercises}/>
-    <Part content = "Using props to pass data" exercices = {parts[1].exercises}/>
-    <Part content = "State of a component" exercices = {parts[2].exercises} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  const {parts} = props
-  return (
-    <div>
-      <p>Number of exercices is {parts[0].exercises+parts[1].exercises+parts[2].exercises}</p>
-    </div>
-
-  )
-}
-
-
-
-
-
-const Display = ({counter}) => <div> {counter} </div> 
-
 
 
 
 const Button = ({ onSmash, text }) => <button onClick={onSmash}>{text}</button>
 
-
-const History = ({allClicks}) => {
-
-  if(allClicks.length==0){
-    return (
-      <div>the application works by clicking</div>
-    )
-  }
-  
-    return (
-      <div>
-          <br></br>
-          <h1>Button Press history : {allClicks.join(' ')}</h1>
-      </div>
-    )
-}
 
 
 

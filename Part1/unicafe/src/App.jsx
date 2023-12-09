495e006eb378196a83df23b4923d7ca73deddc55
YOUNES EL KACIMI <youns.elkacimi@gmail.com>
2023-12-09 12:37:14 +0100

    Exercice 1.7

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -28,43 +28,49 @@ const App = () => {
   const [good,setGood] = useState(0)
   const [neutral,setNeutral] = useState(0)
   const [bad,setBad] = useState(0)
-
-  const onClickGood = () => {
-    setGood(good+1)
-  }
   
-  const onClickNeutral = () => {
-    setNeutral(neutral+1)
-  }
+  const all = good + neutral + bad
+  console.log("all = ",all)
+  const average = (all>0) ? (good-bad)/all:0
+  const positive  = (all>0) ? (good / all)*100:0
   
-  const onClickBad = () => {
-    setBad(bad+1)
-  }
-
-
   return (
     <div>
         <Header text="Give feedback"/>
         
-        <Button onSmash={onClickGood} text="good"/>
-        <Button onSmash={onClickNeutral} text="neutral"/>
-        <Button onSmash={onClickBad} text="bad"/>
+        <Button onSmash={() => setGood(good+1)} text="good"/>
+        <Button onSmash={() => setNeutral(neutral+1)} text="neutral"/>
+        <Button onSmash={() => setBad(bad+1)} text="bad"/>
 
         <Header text="Statistics" />
 
         <table>
-          <tr>
-            <td>good</td>
-            <td>{good}</td>
-          </tr>
-          <tr>
-            <td>neutral</td>
-            <td>{neutral}</td>
-          </tr>
-          <tr>
-            <td>bad</td>
-            <td>{bad}</td>
-          </tr>
+          <tbody>
+            <tr>
+              <td>good</td>
+              <td>{good}</td>
+            </tr>
+            <tr>
+              <td>neutral</td>
+              <td>{neutral}</td>
+            </tr>
+            <tr>
+              <td>bad</td>
+              <td>{bad}</td>
+            </tr>
+            <tr>
+              <td>all</td>
+              <td>{all}</td>
+            </tr>
+            <tr>
+              <td>average</td>
+              <td>{average}</td>
+            </tr>
+            <tr>
+              <td>positive</td>
+              <td>{positive} %</td>
+            </tr>
+          </tbody>
         </table>
 
 

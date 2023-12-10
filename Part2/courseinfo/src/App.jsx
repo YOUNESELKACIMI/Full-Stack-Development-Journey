36beac62c4672077bf6f759e75f858f914d5406b
YOUNES EL KACIMI <youns.elkacimi@gmail.com>
2023-12-10 20:33:12 +0100

    Exercice 2.5

--- Part2/courseinfo/src/App.jsx
+++ Part2/courseinfo/src/App.jsx
@@ -1,65 +1,6 @@
 import { useState } from "react"
 
-
-const Course = ({courses}) => {
-
-  console.log("course in Course Component is ", courses)
-  
-  return (
-    <div> 
-      {courses.map(course => <Header course={course}/>)}
-    </div>
-  )
-}
-
-
-
-
-const Header = ({course}) =>{
-  console.log("Header props = ",course)
-  return (
-    <div>
-      <h1>Course is  {course.name}</h1>
-      <Content parts={course.parts}/>
-      <Sum parts = {course.parts}/>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  const {parts} = props;
-  console.log(parts)
-  return (
-    <div>
-      {parts.map(part => <Part key ={part.id} content={part.name} exercices={part.exercises}/>)}
-    </div>
-  )
-}
-
-
-
-
-const Part = (props) => {
-
-  return (
-    <div>
-      <p>{props.content} {props.exercices} </p>
-    </div>
-  )
-}
-
-
-const Sum = (props) => {
-  const {parts} = props
-  const initialValue = 0
-  console.log("parts in Sum Component is ",parts)
-  return (
-    <div>
-      total of {parts.reduce((s,p) => s+p.exercises,initialValue, )} exercises
-    </div>
-  )
-}
-
+import Course from "./components/Course"
 
 
 const App = () => {

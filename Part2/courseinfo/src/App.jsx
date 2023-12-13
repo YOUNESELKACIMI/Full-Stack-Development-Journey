366cd6693e84d51ed9aae26c23fae7be73107238
YOUNES EL KACIMI <youns.elkacimi@gmail.com>
2023-12-13 14:33:28 +0100

    Exercice 2.11

--- Part2/courseinfo/src/App.jsx
+++ Part2/courseinfo/src/App.jsx
@@ -1,64 +1,41 @@
-import { useState } from "react"
+import { useState,useEffect } from "react"
+import axios from 'axios'
+import Note from "./components/Note"
 
-import Course from "./components/Course"
+
 
 
 const App = () => {
-
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+  const [notes,setNotes] = useState([])
+  const [newNote,setNewNote] = useState('')
+  const [showAll,setShowAll] = useState(true)
 
 
+  useEffect(()=>{
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log("promise fullfiled")
+        console.log(response.data)
+        setNotes(response.data)
+      })
+  },[])
 
+  console.log('render',notes.length,'notes')
 
-
+  
   return (
-      <Course courses = {courses}/>
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+    </div>
   )
 }
-
 
 
 export default App
--- Part2/courseinfo/src/main.jsx
+++ Part2/courseinfo/src/main.jsx
@@ -1,7 +1,4 @@
-import ReactDom from 'react-dom/client'
-import App from './App'
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-
-
-
-ReactDom.createRoot(document.getElementById('root')).render(<App/>)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);

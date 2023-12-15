588963ea991cdd0d6dbbfdf874853eba24480b80
YOUNES EL KACIMI <youns.elkacimi@gmail.com>
2023-12-15 12:18:33 +0100

    Exercice 2.14

--- Part2/phonebook/src/components/Persons.jsx
+++ Part2/phonebook/src/components/Persons.jsx
@@ -1,18 +1,20 @@
 
-const Persons = ({persons,newFilter}) => {
+const Persons = ({persons,newFilter,remove}) => {
   
   return (
     <div>
-    <ul>{persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase())===true).map(filteredPerson => <Person key = {filteredPerson.name} person={filteredPerson} />)}</ul>
+    <ul>{persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase())===true).map(filteredPerson => <Person key = {filteredPerson.name} person={filteredPerson} remove={remove}/>)}</ul>
   </div>
   )
 }
 
 
-const Person = ({ person }) => {
+const Person = ({ person, remove}) => {
     console.log("the person object in the Person component",person)
     return (
-      <li>{person.name} {person.number}</li>
+      <div>
+      <li>{person.name} {person.number}<button onClick={()=>remove(person.id)}>delete</button></li>
+      </div>
     )
   }
   
---
+++ Part2/phonebook/src/services/persons.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const baseUrl = 'http://localhost:3001/persons'
+
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request.then(response=>response.data)
+}
+
+
+const create = (personObject) => {
+    const request = axios.post(baseUrl,personObject)
+    return request.then(response=>response.data)
+}
+
+const remove = (id) =>{
+    console.log("attempt to delete a person inside remove")
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request.then(response=>response.data)
+} 
+
+
+export default {getAll,create,remove}

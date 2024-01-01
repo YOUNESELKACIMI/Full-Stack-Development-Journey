f2580cdb2bbfde44282d590ff2638d7f49e28bd2
YOUNES EL KACIMI <youns.elkacimi@gmail.com>
2024-01-01 14:12:43 +0100

    Exercice 3.5

--- Part3/phonebook/index.js
+++ Part3/phonebook/index.js
@@ -2,7 +2,7 @@ console.log("hello world")
 
 const express = require('express')
 const app= express()
-
+app.use(express.json())
 
 let persons = [
     { 
@@ -55,6 +55,36 @@ app.delete('/api/persons/:id',(req,res)=>{
     persons = persons.filter(person=>person.id!==id)
     console.log("this is the updated persons after deletion",persons)
     res.status(204).end()
+})
+
+const generateId = ()=>{
+    const MaxId = persons.length>0?Math.max(...persons.map(person=>person.id)):0
+    return MaxId +1
+}
+
+app.post('/api/persons',(req,res)=>{
+
+    const body = req.body
+
+    if(!body.name){
+        return res.status(404).json({
+            error:"name is missing"
+        })
+    }
+    else if(!body.number){
+        return res.status(404).json({
+            error:"number is missing"
+        })
+    }
+    const person = {
+        id:generateId(),
+        name:body.name,
+        number:body.number
+    }
+    console.log("the person posted is",person)
+    persons = persons.concat(person)
+    res.json(person)
+    
 })
 
 const PORT = 3001

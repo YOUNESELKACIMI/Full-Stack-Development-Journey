9d7b35667dba426cd71bfacef30edbd2d2284f81
YOUNES EL KACIMI <youns.elkacimi@gmail.com>
2024-01-01 13:45:19 +0100

    Exercice 3.4

--- Part3/phonebook/index.js
+++ Part3/phonebook/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app= express()
 
 
-const persons = [
+let persons = [
     { 
       "id": 1,
       "name": "Arto Hellas", 
@@ -47,7 +47,14 @@ app.get('/api/persons/:id',(request,response)=>{
         })
     }
     response.json(person)
+})
 
+
+app.delete('/api/persons/:id',(req,res)=>{
+    const id = Number(req.params.id)
+    persons = persons.filter(person=>person.id!==id)
+    console.log("this is the updated persons after deletion",persons)
+    res.status(204).end()
 })
 
 const PORT = 3001
